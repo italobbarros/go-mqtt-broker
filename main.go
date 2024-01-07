1da38624ccd0837c00d3fccbb5de47ac58ee10b9
Italo Barros <italo.barros@ativasolucoes.com.br>
2024-01-07 20:54:34 -0300

    feat: add logger library

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"github.com/italobbarros/go-mqtt-broker/internal/api"
 	brokerMqtt "github.com/italobbarros/go-mqtt-broker/internal/broker"
 	connection "github.com/italobbarros/go-mqtt-broker/pkg/connection"
+	"github.com/italobbarros/go-mqtt-broker/pkg/logger"
 )
 
 func main() {
+	logger.InitCustomFormatter()
 	broker := brokerMqtt.NewBroker(&brokerMqtt.BrokerConfigs{
 		Name:           "go-mqtt-broker",
 		Address:        "0.0.0.0:1883",
