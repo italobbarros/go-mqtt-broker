f37af9a54f4bfb06bc1637253c9bdbc6b1ec36cd
Italo Barros <italo.barros@ativasolucoes.com.br>
2024-01-10 23:42:53 -0300

    feat: add auto reload and lasUpdated time

--- frontend/script.js
+++ frontend/script.js
@@ -135,34 +135,53 @@ function processTopic(topic) {
   // Verifica se a string contém '#'
   if (topic.includes('#')) {
     // Substitui todos os '#' por '%23'
+    console.log("processTopic: "+topic)
     return topic.replace(/#/g, '%23');
   }
   // Retorna a string original se não houver '#'
   return topic;
 }
 
+
 function showSidebarDetails(name) {
   const sidebarTitle = document.getElementById('sidebarTitle');
   const sidebarContent = document.getElementById('sidebarContent');
+  const lastUpdatedTime = document.getElementById('lastUpdatedTime'); // Novo elemento para mostrar a última atualização
+
+
+  function getCurrentDateTime() {
+    const currentDate = new Date();
+    return currentDate.toLocaleString(); // Isso irá formatar para a data e hora local
+  }
+
   sidebarContent.innerHTML = '';
 
   sidebarTitle.textContent = `Detalhes de ${name}`;
-  //sidebarContent.textContent = `Informações detalhadas sobre ${name}.`; 
-  
-  fetch(`${TOPIC}${processTopic(name)}`)
-  .then(response => {
-      if (!response.ok) {
-          throw new Error('Erro ao recuperar as informações do tópico');
-      }
-      return response.json();
-  })
-  .then(data => {
-    renderTopicInfo(data);
-  })
-  .catch(error => {
-      console.error('Erro:', error);
-  });
+  if (window.intervalId) {
+    clearInterval(window.intervalId);
+  }
+  const reloadSelect = document.getElementById('reloadSelect');
+  fetchTopicInfo();
+  // Função para buscar informações do tópico
+  function fetchTopicInfo() {
+    lastUpdatedTime.textContent = getCurrentDateTime();
 
+    fetch(`${TOPIC}${processTopic(name)}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao recuperar as informações do tópico');
+        }
+        return response.json();
+    })
+    .then(data => {
+      renderTopicInfo(data);
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+    });
+  }
+
+  // Função para renderizar as informações do tópico
   function renderTopicInfo(topicInfo) {
     const sidebar = document.getElementById('sidebarContent');
     sidebarContent.innerHTML = '';
@@ -184,7 +203,6 @@ function showSidebarDetails(name) {
 
     const subscribers = document.createElement('p');
     subscribers.textContent = `Número de Subscribers: ${topicInfo.subscribersCount}`;
-    console.log("Tópico não encontrado para o nome:", topicInfo.subscribersCount);
     // Adicionar elementos à sidebar
     sidebar.appendChild(topicName);
     sidebar.appendChild(messageCount);
@@ -193,6 +211,30 @@ function showSidebarDetails(name) {
     sidebar.appendChild(retained);
     sidebar.appendChild(subscribers);
   }
+
+  // Adiciona um listener para detectar mudanças no valor selecionado
+  reloadSelect.addEventListener('change', function() {
+    const selectedValue = this.value; // Valor selecionado pelo usuário em segundos
+
+    // Cancela qualquer intervalo anterior se existir
+    if (window.intervalId) {
+      clearInterval(window.intervalId);
+    }
+
+    // Inicia o intervalo com base no valor selecionado
+    window.intervalId = setInterval(fetchTopicInfo, selectedValue * 1000); // Converte segundos para milissegundos
+  });
+
+  // Chama a função fetchTopicInfo inicialmente com base no valor padrão selecionado
+  const initialSelectedValue = reloadSelect.value; // Valor padrão selecionado
+  window.intervalId = setInterval(fetchTopicInfo, initialSelectedValue * 1000); // Converte segundos para milissegundos
+
+  // Obtenha a data e hora atual
+  const currentDate = new Date();
+  const currentTime = currentDate.toLocaleTimeString();
+
+  // Atualize o elemento da última atualização com a hora atual
+  lastUpdatedTime.textContent = currentTime;
   document.getElementById('sidebar').classList.add('active');
 }
 
