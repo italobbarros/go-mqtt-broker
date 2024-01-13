23ff564605dcf537ae47ff3f8298a126385f7cc1
Italo Barros <italo.barros@ativasolucoes.com.br>
2024-01-13 20:13:32 -0300

    feat: add client-tester

--- client.py
+++ client.py
@@ -13,12 +13,15 @@ formatter = logging.Formatter('%(asctime)s - [%(levelname)s] %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+MQTT_NUM_MSG = 10000
+MQTT_NUM_CLIENTS =5
+#823.0356823416419 - 10000 - 2
 async def publish_messages(client_id, topic,delay):
     await asyncio.sleep(delay)
     try:
         async with Client("localhost", 1883,client_id=client_id,timeout=60) as client: #broker.hivemq.com
             for _ in range(1): # quantas vezes repetira
-                for i in range(1, 101):  # Publica 1000 mensagens em topicos diferentes
+                for i in range(MQTT_NUM_MSG):  # Publica 1000 mensagens em topicos diferentes
                     message = f"Mensagem {i} do {client_id}"
                     topic = f"mqtt/{client_id}/topic{i}"
                     await client.publish(topic, message,qos=1)
@@ -27,17 +30,22 @@ async def publish_messages(client_id, topic,delay):
     except Exception as e:
         print(f"Erro de conexão para o Cliente_{client_id}: {e}")
 
+def report(duration):
+    logger.info(f"Tempo total de execução: {duration}")
+    logger.info(f"Qtd_msg: {MQTT_NUM_MSG} sendo enviadas por qtd_clients: {MQTT_NUM_CLIENTS}")
+    logger.info(f"rate: {(MQTT_NUM_MSG*MQTT_NUM_CLIENTS)/duration.total_seconds()} msg/s")
+
 async def main():
     start_time = datetime.datetime.now()
     tasks = []
     # Cria x clientes assíncronos
-    for i in range(1, 501):
+    for i in range(MQTT_NUM_CLIENTS):
         clientId = f"clientId{i}"
         tasks.append(asyncio.create_task(publish_messages(clientId, None,0)))
     
     await asyncio.gather(*tasks)
     duration = datetime.datetime.now() - start_time
-    logger.debug(f"Tempo total de execução: {duration}")
+    report(duration)
 
 if __name__ == "__main__":
     # Change to the "Selector" event loop if platform is Windows
