4e8a22434989a8c1ca023c8647d4d72296e4454e
Italo Barros <italo.barros@ativasolucoes.com.br>
2024-01-14 12:11:31 -0300

    feat: save result on file

--- client.py
+++ client.py
@@ -9,13 +9,20 @@ import logging
 logger = logging.getLogger("Client")
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
+handlerFile = logging.FileHandler("client_mqtt_stats.log")
 formatter = logging.Formatter('%(asctime)s - [%(levelname)s] %(message)s')
+handlerFile.setFormatter(formatter)
 handler.setFormatter(formatter)
+handlerFile.setLevel(logging.INFO)  # Defina o nível do handler de arquivo para INFO ou superior
 logger.addHandler(handler)
+logger.addHandler(handlerFile)
 
 MQTT_NUM_MSG = 100
 MQTT_NUM_CLIENTS =500
-#823.0356823416419 - 10000 - 2
+"""
+1781.8587252596933 msg/s
+
+"""
 async def publish_messages(client_id, topic,delay):
     await asyncio.sleep(delay)
     try:
@@ -31,21 +38,23 @@ async def publish_messages(client_id, topic,delay):
         print(f"Erro de conexão para o Cliente_{client_id}: {e}")
 
 def report(duration):
-    logger.info(f"Tempo total de execução: {duration}")
+    logger.info(f"Tempo total de execucao: {duration}")
     logger.info(f"Qtd_msg: {MQTT_NUM_MSG} sendo enviadas por qtd_clients: {MQTT_NUM_CLIENTS}")
     logger.info(f"rate: {(MQTT_NUM_MSG*MQTT_NUM_CLIENTS)/duration.total_seconds()} msg/s")
 
 async def main():
-    start_time = datetime.datetime.now()
-    tasks = []
-    # Cria x clientes assíncronos
-    for i in range(MQTT_NUM_CLIENTS):
-        clientId = f"clientId{i}"
-        tasks.append(asyncio.create_task(publish_messages(clientId, None,0)))
-    
-    await asyncio.gather(*tasks)
-    duration = datetime.datetime.now() - start_time
-    report(duration)
+    for _ in range(2):
+        start_time = datetime.datetime.now()
+        tasks = []
+        # Cria x clientes assíncronos
+        for i in range(MQTT_NUM_CLIENTS):
+            clientId = f"clientId{i}"
+            tasks.append(asyncio.create_task(publish_messages(clientId, None,0)))
+        
+        await asyncio.gather(*tasks)
+        duration = datetime.datetime.now() - start_time
+        report(duration)
+        await asyncio.sleep(2)
 
 if __name__ == "__main__":
     # Change to the "Selector" event loop if platform is Windows
